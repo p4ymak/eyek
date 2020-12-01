7efc2ac81f0d1af4028f5b593df3142d57a2ab51
Roman Chumak <p4@phygitalism.com>
2020-12-01 14:59:31 +0300

    barycentric transforms added

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,23 @@ impl Tris2D {
         //return min_x, min_y, max_x, max_y of triangle
         [coords_x[0], coords_y[0], coords_x[2], coords_y[2]]
     }
+    fn cartesian_to_barycentric(&self, pt: Point3<f32>) -> Point3<f32> {
+        let v0 = self.b - self.a;
+        let v1 = self.c - self.a;
+        let v2 = pt - self.a;
+        let den = 1.0 / (v0.x * v1.y - v1.x * v0.y);
+
+        let v = (v2.x * v1.y - v1.x * v2.y) * den;
+        let w = (v0.x * v2.y - v2.x * v0.y) * den;
+        let u = 1.0 - v - w;
+        Point3::new(u, v, w)
+    }
+    fn barycentric_to_cartesian(&self, pt: Point3<f32>) -> Point3<f32> {
+        let x = pt.x * self.a.x + pt.y * self.b.x + pt.z * self.c.x;
+        let y = pt.x * self.a.y + pt.y * self.b.y + pt.z * self.c.y;
+        let z = pt.x * self.a.z + pt.y * self.b.z + pt.z * self.c.z;
+        Point3::new(x, y, z)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -199,5 +216,15 @@ fn main() {
     //println!("{:?} {:?} {:?}", inter.distance, inter.u, inter.v);
     println!("Intersects: {:?}", bvh.traverse(&ray, &faces).len());
     println!("{:?}", faces.len());
-    println!("{:?}", cameras);
+
+    let p = Point3::new(0.25, -0.45, 0.0);
+    let a = Point3::new(0.0, 0.0, 0.0);
+    let b = Point3::new(1.0, 0.0, 0.0);
+    let c = Point3::new(1.0, 1.0, 0.0);
+    let tr = Tris2D { a: a, b: b, c: c };
+
+    let bary = tr.cartesian_to_barycentric(p);
+    let cart = tr.barycentric_to_cartesian(bary);
+
+    println!("P {:?} \nBary {:?}\nP'{:?}", p, bary, cart);
 }
